030562db125e3a596ce19a5780fccd6726535c6b
NaraeJang <j.naaaarae@gmail.com>
2023-03-06 15:22:49 -0500

    Set up mongoDB for this project

--- app.js
+++ app.js
@@ -9,6 +9,39 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.use(express.static("public"));
+
+
+
+
+//////////MongoDB set up//////////
+mongoose.connect('mongodb://127.0.0.1:27017/wikiDB', {
+    useNewUrlParser: true
+});
+
+const articleSchema = new mongoose.Schema({
+    title: String,
+    Content: String
+});
+
+const Article = new mongoose.model('Article', articleSchema);
+
+
+
+
+
+//////////Building a Website starts from here//////////
+
+
+
+
+
+
+//////////Port Message//////////
+app.listen(3000, () => {
+    console.log('Server started on port 3000.');
+});
