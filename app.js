a2a3da307e6a0b176277dada80d83e153c86feec
NaraeJang <j.naaaarae@gmail.com>
2023-03-07 14:52:18 -0500

    Chained Route Handlers Using Express

--- app.js
+++ app.js
@@ -36,48 +36,42 @@ const Article = new mongoose.model('Article', articleSchema);
 
 
 
-//////////RESTful API: GET(Fetch all the articles)//////////
-app.get("/articles", function (req, res) {
-    Article.find().then(foundArticles => {
-        res.send(foundArticles);
-    }).catch(error => {
-        console.log('found errors while fetching data');
+
+app.route("/articles")
+
+    .get(function (req, res) {
+        Article.find().then(foundArticles => {
+            res.send(foundArticles);
+        }).catch(error => {
+            console.log('found errors while fetching data');
+        });
+    })
+
+    .post(function (req, res) {
+
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Successfully added a new article.");
+            } else {
+                res.send(err);
+            }
+        });
+
+    })
+    
+    .delete(function (req, res) {
+        Article.deleteMany().then(result => {
+            res.send("Seccessfully deleted all articles.");
+        }).catch(err => {
+            console.log(err);
+        });
     });
-});
 
-
-
-
-//////////RESTful API: POST(Create one new article)//////////
-app.post("/articles", function (req, res) {
-
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
-    });
-
-    newArticle.save(function (err) {
-        if (!err) {
-            res.send("Successfully added a new article.");
-        } else {
-            res.send(err);
-        }
-    });
-
-});
-
-
-
-
-
-//////////RESTful API: DELETE//////////
-app.delete("/articles", function (req, res) {
-    Article.deleteMany().then(result => {
-        res.send("Seccessfully deleted all articles.");
-    }).catch(err => {
-        console.log(err);
-    });
-});
 
 
 
