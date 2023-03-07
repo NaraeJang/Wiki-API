0c8401e476d886ccd7785ab12f07e841e480dc47
NaraeJang <j.naaaarae@gmail.com>
2023-03-07 12:48:39 -0500

    Post a New Article

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wiki API
+This project is my first project of building my own RESTful API.
+
+# What I learned?
+- About REST
+- MongoDB COMPASS
+- Reviewing Seting up server
+- Get all articles
+- Post a new articles
+- Delete all articles
+- Chained Route Handlers Using Express
+- GET a Specific Article
+- PUT a Specific Article
+- PATCH a Specific Article
+- DELETE a Specific Article
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,6 +19,7 @@ app.use(express.static("public"));
 
 
 
+
 //////////MongoDB set up//////////
 mongoose.connect('mongodb://127.0.0.1:27017/wikiDB', {
     useNewUrlParser: true
@@ -25,7 +27,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/wikiDB', {
 
 const articleSchema = new mongoose.Schema({
     title: String,
-    Content: String
+    content: String
 });
 
 const Article = new mongoose.model('Article', articleSchema);
@@ -34,8 +36,44 @@ const Article = new mongoose.model('Article', articleSchema);
 
 
 
-//////////Building a Website starts from here//////////
+//////////RESTful API: GET(Fetch all the articles)//////////
+app.get("/articles", function (req, res) {
+    Article.find().then(foundArticles => {
+        res.send(foundArticles);
+    }).catch(error => {
+        console.log('found errors while fetching data');
+    });
+});
 
+
+
+
+//////////RESTful API: POST(Create one new article)//////////
+app.post("/articles", function (req, res) {
+
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save(function (err) {
+        if (!err) {
+            res.send("Successfully added a new article.");
+        } else {
+            res.send(err);
+        }
+    });
+
+});
+
+
+
+
+
+//////////RESTful API: DELETE//////////
+app.delete("/articles", function (req, res) {
+
+});
 
 
 
